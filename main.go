226757afefbec8c65fdcddf98385739e16761d10
Ajay kumar <aju33146@gmail.com>
2025-02-06 04:53:49 +0530

    new course added

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("hello golang")
+	var investAmount = 1000
+	var returnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investAmount) * math.Pow(1+returnRate/100, float64(years))
+	fmt.Println(futureValue)
 }
